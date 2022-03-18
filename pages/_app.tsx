07da1727dd89b87ebfcbefed7375b64e824547c6
CarbonGhost <39361743+CarbonGhost@users.noreply.github.com>
2022-03-18 22:39:07 +0800

    fix: Resolve build
    Resolve issue that caused an "no-document-import-in-page" error when building.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
+      <Head>
+        <title>Prosperity</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="description" content="The Prosperity Minecraft server." />
+        <meta
+          name="keywords"
+          content="Prosperity, Prosperity MC, Prosperity SMP, Prosperity Wiki, Minecraft, Minecraft server"
+        />
+        <meta property="og:site_name" content="Prosperity MC" />
+        <meta name="theme-color" content="#FFC10B" />
+        <meta property="og:type" content="website" />
+      </Head>
       <Nav />
       <div className="mt-[6.5rem] responsive-width">
         <Component {...pageProps} />
@@ -9,6 +25,7 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
   );
 }
 
+import Head from "next/head";
 import Nav from "@components/nav";
 import type { AppProps } from "next/app";
 import "tailwindcss/tailwind.css";
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,25 +2,10 @@ export default function Document(): JSX.Element {
   return (
     <Html>
       <Head>
-        <title>Prosperity</title>
         <link
           rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=jetbrains+mono|montserrat|inter"
+          href="https://fonts.googleapis.com/css?family=jetbrains+mono|montserrat|inter&display=optional"
         />
-
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"
-        ></meta>
-        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="description" content="The Prosperity Minecraft server." />
-        <meta
-          name="keywords"
-          content="Prosperity, Prosperity MC, Prosperity SMP, Prosperity Wiki, Minecraft, Minecraft server"
-        />
-        <meta property="og:site_name" content="Prosperity MC" />
-        <meta name="theme-color" content="#FFC10B" />
-        <meta property="og:type" content="website" />
       </Head>
       <body>
         <Main />
