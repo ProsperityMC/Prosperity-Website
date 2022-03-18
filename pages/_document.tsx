8eecccfb1b7feeb5d0252fe791ca1d07a7e8d293
CarbonGhost <39361743+CarbonGhost@users.noreply.github.com>
2022-03-18 22:51:33 +0800

    fix: Fix Google fonts API
    Resolve Google API causing build failure.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document(): JSX.Element {
       <Head>
         <link
           rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=jetbrains+mono|montserrat|inter&display=optional"
+          href="https://fonts.googleapis.com/css?family=JetBrains+Mono|Montserrat:100,200,300,400,500,600,700,800,900,i|Inter:100,200,300,400,500,600,700,800,900,i&display=optional"
         />
       </Head>
       <body>
