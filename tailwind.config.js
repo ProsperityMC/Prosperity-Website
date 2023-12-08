34d22cdcc2caad5512b97ed5c564fa8f32099f26
CarbonGhost <39361743+CarbonGhost@users.noreply.github.com>
2023-12-08 16:33:10 +0800

    improve: Update palettes

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,33 @@ module.exports = {
 				sans: ["var(--font-atkinson-hyperlegible)"],
 				header: ["var(--font-inter)"],
 				mono: ["var(--font-jetbrains-mono)"]
+			},
+			colors: {
+				yellow: {
+					50: "#fff9e8",
+					100: "#fff3d1",
+					200: "#ffe6a4",
+					300: "#feda76",
+					400: "#fecd49",
+					500: "#fec11b",
+					600: "#cb9a16",
+					700: "#987410",
+					800: "#664d0b",
+					900: "#332705"
+				},
+				zinc: {
+					50: "#F2F2F3",
+					100: "#E4E6E7",
+					200: "#C6C9CC",
+					300: "#ACB0B4",
+					400: "#91969C",
+					500: "#747A81",
+					600: "#5B6166",
+					700: "#43474B",
+					800: "#292B2E",
+					900: "#111213",
+					950: "#070808"
+				}
 			}
 		}
 	},
