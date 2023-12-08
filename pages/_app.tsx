74cfccbefb33b91e417f5bcd0573bc1e1ecc5ec6
CarbonGhost <39361743+CarbonGhost@users.noreply.github.com>
2023-12-08 17:54:09 +0800

    new: Add Vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable camelcase */
 import { AppProps } from "next/app";
 import "@styles/globals.css";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Layout from "@components/Layout";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
@@ -19,6 +20,8 @@ export default function App({ Component, pageProps }: AppProps) {
 				<meta name="theme-color" content="#fec11b" />
 				<meta property="og:image" content="_next/image?url=%2Fopengraph-image.png&w=3840&q=100" />
 			</Head>
+			<Analytics />
+			<SpeedInsights />
 			<Component {...pageProps} />
 		</Layout>
 	);
