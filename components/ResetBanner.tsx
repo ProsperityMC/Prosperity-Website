e75ed6357b851ec00902154c06cd255f73727338
CarbonGhost <39361743+CarbonGhost@users.noreply.github.com>
2024-09-03 18:58:46 +0800

    improve: Refine wiki index and banner styles

--- components/ResetBanner.tsx
+++ components/ResetBanner.tsx
@@ -2,14 +2,16 @@ import A from "./A";
 
 export default function ResetBanner() {
 	return (
-		<div className="bg-yellow-500/30 text-yellow-500 border border-yellow-500/20 px-6 py-1.5 rounded-md backdrop-blur-sm">
-			A server reset is planned for the near future, make a suggestion or
-			ask a question on{" "}
-			<A
-				className="underline hover:no-underline"
-				href="https://discord.com/invite/hfTxZ4XxYj">
-				Discord
-			</A>
+		<div className="bg-purple-500/30 text-purple-200 border border-purple-500/20 px-6 py-1.5 rounded-md backdrop-blur-sm">
+			<p>
+				A server reset is planned for the near future, make a suggestion or
+				ask a question on{" "}
+				<A
+					className="underline hover:no-underline"
+					href="https://discord.com/invite/hfTxZ4XxYj">
+					Discord
+				</A>
+			</p>
 		</div>
 	);
 }
--- pages/wiki/index.tsx
+++ pages/wiki/index.tsx
@@ -6,6 +6,7 @@ import { wikiPageDataGetAll } from "@lib/lib";
 import WikiMenu from "@components/WikiMenu";
 import Index from "@wiki/index.mdx";
 import ResetBanner from "@components/ResetBanner";
+import { DocumentIcon, SparklesIcon } from "@heroicons/react/24/outline";
 
 export async function getStaticProps() {
 	const data = await wikiPageDataGetAll();
@@ -54,10 +55,13 @@ export default function Wiki({
 						.map((page) => (
 							<A
 								href={`/wiki/${page.slug}`}
-								className="md:col-span-2 p-4 pb-6 block bg-gradient-to-br from-yellow-500/30 to-zinc-800/30 hover:bg-yellow-500/20 text-left rounded-md duration-200 transform focus:border-yellow-400 border border-yellow-500 w-full">
-								<header className="text-lg type-header my-1.5 text-yellow-500">
-									{page.meta?.title || page.slug}
-								</header>
+								className="md:col-span-2 p-4 pb-6 block bg-gradient-to-bl from-yellow-500/30 to-zinc-800/30 hover:bg-yellow-500/20 text-left rounded-md duration-200 transform focus:border-yellow-400 border border-yellow-500 w-full">
+								<div className="flex gap-2">
+									<SparklesIcon className="w-6 h-6 inline-block mt-2 text-yellow-500"></SparklesIcon>
+									<header className="text-lg type-header my-1.5 text-yellow-500">
+										{page.meta?.title || page.slug}
+									</header>
+								</div>
 								<p className="mb-1.4 max-w-lg text-yellow-500">
 									{page.meta?.short || ""}
 								</p>
@@ -72,10 +76,13 @@ export default function Wiki({
 						.map((page) => (
 							<A
 								href={`/wiki/${page.slug}`}
-								className="p-4 pb-6 block hover:bg-zinc-900 text-left rounded-md duration-200 transform focus:border-yellow-400 bg-zinc-800 border border-zinc-700 w-full">
-								<header className="text-lg type-header my-1.5">
-									{page.meta?.title || page.slug}
-								</header>
+								className="p-4 pb-6 hover:bg-zinc-900 text-left rounded-md duration-200 transform focus:border-yellow-400 bg-zinc-800 border border-zinc-700 w-full">
+								<div className="flex gap-2">
+									<DocumentIcon className="w-6 h-6 inline-block mt-2"></DocumentIcon>
+									<header className="text-lg type-header my-1.5">
+										{page.meta?.title || page.slug}
+									</header>
+								</div>
 								<p className="mb-1.4 max-w-sm">
 									{page.meta?.short || ""}
 								</p>
