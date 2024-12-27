03c0b3347fbd6d466a1b1f6c7b12dd678cef9e54
CarbonGhost <39361743+CarbonGhost@users.noreply.github.com>
2024-12-27 13:00:34 +0800

    fix: Fix wrong path

--- pages/join.tsx
+++ pages/join.tsx
@@ -1,11 +1,17 @@
-import ArticleContent from "@wiki/join-info-v2.mdx";
+import ArticleContent from "@wiki/member-requirements.mdx";
 
 export default function Join() {
 	return (
-		<main className="markdown">
-			<section className="flex flex-col gap-10 text-center markdown"></section>
-			<hr />
-			<article className="markdown">
+		<main>
+			<article className="w-full markdown max-w-[100ch] mx-auto min-h-screen">
+				<section className="markdown text-center">
+					<header className="text-4xl">How to Join</header>
+					<p className="text-lg text-zinc-300">
+						Requirements and recommendations for joining the
+						community.
+					</p>
+				</section>
+				<hr />
 				<ArticleContent></ArticleContent>
 			</article>
 		</main>
